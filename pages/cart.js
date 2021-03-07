5284a62e9d70416b58eaa5bb7bc7d8b4cb9381c1
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2021-03-08 01:48:18 +0700

    feature: [list cart item]

--- pages/cart.js
+++ pages/cart.js
@@ -1,7 +1,106 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useContext, useState, useEffect } from 'react'
+import { DataContext } from '../store/GlobalState'
+import CartItem from 'components/CartItem'
+import { getData } from 'utils/fetchData'
+
 function Cart() {
+  const { state, dispatch } = useContext(DataContext)
+  const { cart, auth, } = state
+  const [address, setAddress] = useState('')
+  const [mobile, setMobile] = useState('')
+  const [total, setTotal] = useState(0)
+
+  const [callback, setCallback] = useState(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    const getTotal = () => {
+      const res = cart.reduce((prev, item) => {
+        return prev + (item.price * item.quantity)
+      }, 0)
+
+      setTotal(res)
+    }
+
+    getTotal()
+  }, [cart])
+
+  useEffect(() => {
+    /** check cart local dengan yang ada di dalam database */
+    const cartLocal = JSON.parse(localStorage.getItem('__next__cart'))
+    if (cartLocal && cartLocal.length > 0) {
+      let newArr = []
+      const updateCart = async () => {
+        for (const item of cartLocal) {
+          const res = await getData(`product/${item._id}`)
+          const { _id, title, images, price, inStock, sold } = res.product
+          // jika stock lebih dari 0 simpan ke cart
+          if (inStock > 0) {
+            newArr.push({
+              _id, title, images, price, inStock, sold,
+              quantity: item.quantity > inStock ? 1 : item.quantity
+            })
+          }
+        }
+
+        dispatch({ type: 'ADD_CART', payload: newArr })
+      }
+
+      updateCart()
+    }
+  }, [callback])
+
+
+
+  if (cart.length === 0)
+    return <img className="img-responsive w-100" src="/empty_cart.jpeg" alt="not empty" />
+
   return (
-    <div>
-      <h1>Cart</h1>
+    <div className="row mx-auto">
+      <Head>
+        <title>Cart Page</title>
+      </Head>
+
+      <div className="col-md-8 text-secondary table-responsive my-3">
+        <h2 className="text-uppercase">Shopping Cart</h2>
+
+        <table className="table my-3">
+          <tbody>
+            {
+              cart.map(item => (
+                <CartItem key={item._id} item={item} dispatch={dispatch} cart={cart} />
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+
+      <div className="col-md-4 my-3 text-right text-uppercase text-secondary">
+        <form>
+          <h2>Shipping</h2>
+
+          <label htmlFor="address">Address</label>
+          <input type="text" name="address" id="address"
+            className="form-control mb-2" value={address}
+            onChange={e => setAddress(e.target.value)} />
+
+          <label htmlFor="mobile">Mobile</label>
+          <input type="text" name="mobile" id="mobile"
+            className="form-control mb-2" value={mobile}
+            onChange={e => setMobile(e.target.value)} />
+        </form>
+
+        <h3>Total: <span className="text-danger">${total}</span></h3>
+
+
+        <Link href={auth.user ? '#!' : '/signin'}>
+          <a className="btn btn-dark my-2" onClick={null}>Proceed with payment</a>
+        </Link>
+
+      </div>
     </div>
   )
 }
