0f63a03f6b384b1a975b2a771e8947eef50e6697
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2021-03-08 02:06:39 +0700

    component: [moda]

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from './NavBar'
 import Notify from './Notify'
+import Modal from './Modal'
 
 function Layout({ children }) {
   return (
     <div className="container">
       <NavBar />
       <Notify />
+      <Modal />
       {children}
     </div>
   )
