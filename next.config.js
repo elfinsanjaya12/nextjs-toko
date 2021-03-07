69e07c20c8c1cec8d13aa678adaeb6706b1fdce6
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2021-03-07 18:48:16 +0700

    setup: [localhost]

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    "BASE_URL": "nextjs-blog-pi-lemon.vercel.app",
+    "BASE_URL": "http://localhost:3000",
     "MONGODB_URL": "mongodb+srv://bwamern:Copyright12@cluster0.lmwrb.mongodb.net/db_e_commerce?retryWrites=true&w=majority",
     "ACCESS_TOKEN_SECRET": "ACCESS_TOKEN_SECRET",
     "REFRESH_TOKEN_SECRET": "REFRESH_TOKEN_SECRET"
